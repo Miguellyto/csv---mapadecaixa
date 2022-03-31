a158181e375708494713faefecb3d0db8246c25c
Luiz Miguel <luizmiguelbelizariodasilva@gmail.com>
2022-03-31 16:16:33 -0300

    Update cod

--- CSV_Editor.py
+++ CSV_Editor.py
@@ -1,15 +1,11 @@
-from cgitb import text
-from distutils.command.upload import upload
 import pandas as pd
 import numpy as np
 import os
-import sys
 import tkinter as tk
 from pathlib import Path
 from  tkinter import*
 import tkinter.messagebox
 from tkinter import filedialog
-# from PIL import ImageTk, Image
 
 def UploadAction():
     filename = filedialog.askopenfilename(initialdir="C:/Users//Downloads/", title="Selecione um Arquivo CSV", filetypes=(("csv files", "*.csv"),("all files", "*.*")))
@@ -17,32 +13,22 @@ def UploadAction():
 
     mapadecaixa = pd.read_csv(filename, encoding='ANSI', sep=',', header=0, thousands = '.', decimal = ',', dtype = {'Valor':np.float64})
 
-    # colunas = {'3o. Agrupamento':'3AGRUPAMENTO', '4o. Agrupamento':'FILIAL', 'Descrição':'DESCRIÇÃO', 'Cliente':'CLIENTE','Crédito':'DEBITO', 'Emissão':'DATA', 'Valor':'VALOR'}
-    colunas = {'3o. Agrupamento':'3AGRUPAMENTO', '4o. Agrupamento':'FILIAL','Descrição':'DESCRIÇÃO','Cliente':'CLIENTE','Crédito':'DEBITO','Emissão':'DATA','Valor':'VALOR'}
-    # colunas = {'3o. Agrupamento':'', '4o. Agrupamento':'FILIAL', 'Descrição':'', 'Cliente':'','Crédito':'DEBITO', 'Emissão':'DATA', 'Valor':'VALOR'}
+    mapadecaixa[['4o. Agrupamento','DEBITO']] = mapadecaixa['4o. Agrupamento'].str.split(';', expand=True)
 
-    mapadecaixa['CREDITO'] = 10164 
-    # mapadecaixa['HISTORICO'] = 10.01
-    mapadecaixa['HISTORICO'] = '10.01'+ "," + mapadecaixa['3o. Agrupamento']+ "," + mapadecaixa['Cliente']+ "," + mapadecaixa['Descrição']
-    # mapadecaixa['HISTORICO'] = '10.01' + mapadecaixa['3o. Agrupamento'] + mapadecaixa['Cliente'] + mapadecaixa['Descrição']
-
-    # mapadecaixa['3o. Agrupamento'] = '3o. Agrupamento'
-    # mapadecaixa['Cliente'] = 'Cliente'
-    # mapadecaixa['Descrição'] = 'Descrição'
-
+    mapadecaixa['FILIAL'] = ''
+    mapadecaixa['CREDITO'] = '10164' 
+    mapadecaixa['HISTORICO'] = '10.01'+ "," + mapadecaixa['4o. Agrupamento']+ "," + mapadecaixa['Cliente']+ "," + mapadecaixa['Descrição']
     mapadecaixa['HISTORICO'] = mapadecaixa['HISTORICO'].str.upper()
-    # mapadecaixa['3o. Agrupamento'] = mapadecaixa['3o. Agrupamento'].str.upper()
-    # mapadecaixa['Cliente'] = mapadecaixa['Cliente'].str.upper()
 
-
+    colunas = {'4o. Agrupamento':'4AGRUPAMENTO','Descrição':'DESCRIÇÃO','Cliente':'CLIENTE','Emissão':'DATA','Valor':'VALOR'}
     mapadecaixa.rename(columns = colunas, inplace=True) 
-    # new_ordem_colls = ['FILIAL', 'DATA', 'DEBITO', 'CREDITO', 'VALOR', 'HISTORICO', '3AGRUPAMENTO', 'CLIENTE', 'DESCRIÇÃO']
-    # new_ordem_colls = ['FILIAL', 'DATA', 'DEBITO', 'CREDITO', 'VALOR', 'HISTORICO', '3o. Agrupamento', 'Cliente', 'Descrição']
+
     new_ordem_colls = ['FILIAL', 'DATA', 'DEBITO', 'CREDITO', 'VALOR', 'HISTORICO']
     mapadecaixa = mapadecaixa[new_ordem_colls]
 
     filial = en.get()
     mapadecaixa['FILIAL'] = filial
+    # mapadecaixa.to_csv('MapaCaixa.csv', index=False)
     mapadecaixa.to_csv('MapaCaixa.csv', encoding='latin-1', index=False) 
 
 # root = tk.Tk()
@@ -101,6 +87,6 @@ root.mainloop()
 
 
 # ------------------------ 
-if  (('3o. Agrupamento' == 'DUPLICATA') end ('3o. Agrupamento' == 'CRÉDITO BANCÁRIO') end ('3o. Agrupamento' == 'CHEQUE À VISTA')):
-    '3o. Agrupamento' + '-' + 'Cliente'
-else:
+# if  (('4o. Agrupamento' == 'DUPLICATA') end ('4o. Agrupamento' == 'CRÉDITO BANCÁRIO') end ('4o. Agrupamento' == 'CHEQUE À VISTA')):
+#     '4o. Agrupamento' + '-' + 'Cliente'
+# else:
